2add04b537d46216672c56b007a977ca5007bf03
audetcameron <audetcameron@users.noreply.github.com>
2025-05-29 15:33:00 +0000

    deploy: 8f5492f9f72e72010f5d815e2292c147c0670a08

--- src/_data/meta.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  url: process.env.URL || "http://localhost:8080",
-  siteName: "Capital Web Company - New Hampshire Small Business Website Solutions",
-  siteDescription:
-    "Because sometimes you just need a simple website. Who you are, what you do, and why people should care. Let's make it simple.",
-  authorName: "capitalwebcompany",
-  twitterUsername: "foo", // no `@`,
-  defaultImage:"https://capitalwebcompany.com/img/og-image.png"
-};
